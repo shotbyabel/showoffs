6e8115c727725b9896f1f12a2d18ef7ccd1d4779
Abel Alejandro <shotbyabel@gmail.com>
2015-04-29 19:40:48 -0700

    added final project files

---
+++ js/carousel.js
@@ -0,0 +1,62 @@
+Element.prototype.carousel = function(){
+
+	var carousel =  this;
+	var wrapper = carousel.children[0];
+	var slides = wrapper.children;
+	var position = 0;
+
+	var leftBtn = document.createElement('div');
+	var rightBtn = document.createElement('div');
+
+
+	var width = window.innerWidth;
+	wrapper.style.width = width  * slides.length + "px";
+
+	this.createButtons = function(){
+
+		leftBtn.classList.add('left');
+		rightBtn.classList.add('right');
+
+		leftBtn.addEventListener('mousedown', function(){
+			position = position + window.innerWidth;
+			wrapper.style.marginLeft = position + "px";
+			console.log(position);
+		});
+
+		rightBtn.addEventListener('mousedown', function(){
+			position = position - window.innerWidth;
+			wrapper.style.marginLeft = position + "px";
+
+		});
+
+
+		slider.appendChild(leftBtn);
+		slider.appendChild(rightBtn);
+
+
+	};
+
+
+	this.resize = function(){
+		wrapper.style.width = width  * slides.length + "px";
+		
+		for(var index=0; index < slides.length; index++){
+		slides[index].style.width = width + "px";	
+	
+		}
+
+		leftBtn.style.top = rightBtn.style.top = (window.innerHeight / 2) + "px";
+		
+
+	};
+
+	this.resize();
+	this.createButtons();
+
+
+
+
+	console.log(slides);
+	
+};
+/* end Slider */
